ea59292c1d41baa5c48095eb720a23aa9b62334b
felipemarq <137407401+felipemarq@users.noreply.github.com>
2023-06-29 11:30:50 +0000

    feat: :sparkles: Added Ambient Variables Validations

--- api/src/modules/auth/auth.module.ts
+++ api/src/modules/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt'
+import { env } from 'src/shared/config/env';
 
 @Module({
   imports: [JwtModule.register({
-    secret: "unsecure_jwt_secret",
+    secret: env.jwtSecret,
     signOptions: { expiresIn: '7d' },
   }),],
   controllers: [AuthController],
