6aa9796f82a250f3da19e3c5d77d5b19dc563a5b
felipemarq <137407401+felipemarq@users.noreply.github.com>
2023-06-30 18:51:28 +0000

    feat: :sparkles: Bank Accounts CRUD completed

--- api/src/shared/database/repositories/bank-accounts.repositories.ts
+++ api/src/shared/database/repositories/bank-accounts.repositories.ts
@@ -9,4 +9,20 @@ export class BankAccountsRepository {
   create(createDto: Prisma.BankAccountCreateArgs) {
     return this.prismaService.bankAccount.create(createDto);
   }
+
+  findMany(findManyDto: Prisma.BankAccountFindManyArgs) {
+    return this.prismaService.bankAccount.findMany(findManyDto);
+  }
+
+  findFirst(findFirstDto: Prisma.BankAccountFindFirstArgs) {
+    return this.prismaService.bankAccount.findFirst(findFirstDto);
+  }
+
+  update(updateDto: Prisma.BankAccountUpdateArgs) {
+    return this.prismaService.bankAccount.update(updateDto);
+  }
+
+  delete(deleteDto: Prisma.BankAccountDeleteArgs) {
+    return this.prismaService.bankAccount.delete(deleteDto);
+  }
 }
