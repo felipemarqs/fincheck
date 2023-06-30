8b9fbe0717d6177389ef218eb99fa518abcb455e
felipemarq <137407401+felipemarq@users.noreply.github.com>
2023-06-30 10:56:28 +0000

    feat: :sparkles: Custom Decorator Created
    Custom decorator created to use in public routes

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -2,19 +2,19 @@ import { Body, Controller, Post, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SigninDto } from './dtos/signin.dto';
 import { SignupDto } from './dtos/signup.dto';
+import { IsPublic } from 'src/shared/decorators/IsPublic';
 
 @Controller('auth')
+@IsPublic() //Custom Decorator
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signin')
-  @SetMetadata('IS_PUBLIC', true)
   signin(@Body() signinDto: SigninDto) {
     return this.authService.signin(signinDto);
   }
 
   @Post('signup')
-  @SetMetadata('IS_PUBLIC', true)
   signup(@Body() signupDto: SignupDto) {
     return this.authService.signup(signupDto);
   }
--- api/src/modules/auth/auth.guard.ts
+++ api/src/modules/auth/auth.guard.ts
@@ -9,13 +9,14 @@ import { JwtService } from '@nestjs/jwt';
 
 import { Request } from 'express';
 import { env } from 'src/shared/config/env';
+import { IS_PUBLIC_KEY } from 'src/shared/decorators/IsPublic';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private jwtService: JwtService, private reflector: Reflector) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const isPublic = this.reflector.getAllAndOverride<boolean>('IS_PUBLIC', [
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getClass(),
       context.getHandler(),
     ]);
