79e9dca1c855b9ba8955047048d15e14fc0c2ca7
felipemarqs <felipe.marqs@outlook.com>
2023-07-02 14:01:18 -0300

    feat: :sparkles: Transaction Modules Created

---
+++ api/src/modules/transactions/transactions.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
+import { TransactionsService } from './transactions.service';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { UpdateTransactionDto } from './dto/update-transaction.dto';
+
+@Controller('transactions')
+export class TransactionsController {
+  constructor(private readonly transactionsService: TransactionsService) {}
+
+  @Post()
+  create(@Body() createTransactionDto: CreateTransactionDto) {
+    return this.transactionsService.create(createTransactionDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.transactionsService.findAll();
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateTransactionDto: UpdateTransactionDto,
+  ) {
+    return this.transactionsService.update(+id, updateTransactionDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.transactionsService.remove(+id);
+  }
+}
---
+++ api/src/modules/transactions/transactions.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { UpdateTransactionDto } from './dto/update-transaction.dto';
+
+@Injectable()
+export class TransactionsService {
+  create(createTransactionDto: CreateTransactionDto) {
+    return 'This action adds a new transaction';
+  }
+
+  findAll() {
+    return `This action returns all transactions`;
+  }
+
+  update(id: number, updateTransactionDto: UpdateTransactionDto) {
+    return `This action updates a #${id} transaction`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} transaction`;
+  }
+}
