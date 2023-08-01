0116341e3256d88e0caeeb992261034c61923e71
felipemarq <137407401+felipemarq@users.noreply.github.com>
2023-08-01 12:42:09 +0000

    feat: :sparkles: LaunchScreen created

--- frontend/src/app/contexts/AuthContext.tsx
+++ frontend/src/app/contexts/AuthContext.tsx
@@ -3,7 +3,7 @@ import { localStorageKeys } from "../config/localStorageKeys";
 import { useQuery } from "@tanstack/react-query";
 import { usersService } from "../services/usersService";
 import { toast } from "react-hot-toast";
-import { PageLoader } from "../../view/components/PageLoader";
+import { LaunchScreen } from "../../view/components/LaunchScreen";
 
 interface AuthContextValue {
   signedIn: boolean;
@@ -32,7 +32,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     setSignedIn(false);
   }, []);
 
-  const { isError, data, isFetching, isSuccess, remove } = useQuery({
+  const { isError, /* data, */ isFetching, isSuccess, remove } = useQuery({
     queryKey: ["users", "me"],
     queryFn: () => usersService.me(),
     enabled: signedIn,
@@ -46,14 +46,11 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     }
   }, [isError, signout]);
 
-  if (isFetching) {
-    return <PageLoader />;
-  }
-
   return (
     <AuthContext.Provider value={{ signedIn: isSuccess && signedIn, signin, signout }}>
-      <h1>{data?.email}</h1>
-      {children}
+      <LaunchScreen isLoading={isFetching} />
+
+      {!isFetching && children}
     </AuthContext.Provider>
   );
 };
