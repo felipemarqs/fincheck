db7250102480d0e87d4a138a8ac9da52d73ad187
felipemarq <137407401+felipemarq@users.noreply.github.com>
2023-08-17 13:34:43 +0000

    att: node version

--- frontend/src/view/pages/Dashboard/components/Accounts/AccountCard.tsx
+++ frontend/src/view/pages/Dashboard/components/Accounts/AccountCard.tsx
@@ -1,14 +1,14 @@
-import { BankAccount } from "../../../../../app/entities/BankAccount";
-import { cn } from "../../../../../app/utils/cn";
-import { formatCurrency } from "../../../../../app/utils/formatCurrency";
-import { BankAccountTypeIcon } from "../../../../components/icons/BankAccountTypeIcon";
-import { useDashboard } from "../DashboardContext/useDashboard";
+import { BankAccount } from '../../../../../app/entities/BankAccount';
+import { cn } from '../../../../../app/utils/cn';
+import { formatCurrency } from '../../../../../app/utils/formatCurrency';
+import { BankAccountTypeIcon } from '../../../../components/icons/BankAccountTypeIcon';
+import { useDashboard } from '../DashboardContext/useDashboard';
 
 interface AccountCardProps {
   data: BankAccount;
 }
 export const AccountCard = ({ data }: AccountCardProps) => {
-  const { color, name, currentBalance, type, id } = data;
+  const { color, name, currentBalance, type } = data;
   const { areValuesVisible, openEditAccountModal } = useDashboard();
   return (
     <div
@@ -20,13 +20,15 @@ export const AccountCard = ({ data }: AccountCardProps) => {
       <div>
         <BankAccountTypeIcon type={type} />
 
-        <span className="text-gray-800 tracking-[-0.5] font-medium mt-4 block">{name}</span>
+        <span className="text-gray-800 tracking-[-0.5] font-medium mt-4 block">
+          {name}
+        </span>
       </div>
       <div>
         <span
           className={cn(
-            "text-gray-800 tracking-[-0.5] font-medium mt-4 block",
-            !areValuesVisible && "blur-sm"
+            'text-gray-800 tracking-[-0.5] font-medium mt-4 block',
+            !areValuesVisible && 'blur-sm'
           )}
         >
           {formatCurrency(currentBalance)}
