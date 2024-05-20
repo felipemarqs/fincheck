c1e24538d28f3b6f4362defab9c4aed5b3002380
felipemarq <137407401+felipemarq@users.noreply.github.com>
2024-05-20 13:38:31 +0000

    refactor: :arrow_up: Dependencies upgraded

--- frontend/src/app/contexts/AuthContext.tsx
+++ frontend/src/app/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useCallback, useEffect, useState } from 'react';
 import { localStorageKeys } from '../config/localStorageKeys';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { usersService } from '../services/usersService';
 import { toast } from 'react-hot-toast';
 import { LaunchScreen } from '../../view/components/LaunchScreen';
@@ -24,6 +24,9 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     return !!storedAccessToken;
   });
 
+  const queryClient = useQueryClient();
+
+
   const signin = useCallback((accessToken: string) => {
     localStorage.setItem(localStorageKeys.ACCESS_TOKEN, accessToken);
 
@@ -32,9 +35,10 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 
   const signout = useCallback(() => {
     localStorage.removeItem(localStorageKeys.ACCESS_TOKEN);
-    remove();
+    queryClient.invalidateQueries()
     setSignedIn(false);
   }, []);
+
 
   const { isError, data, isFetching, isSuccess, remove } = useQuery({
     queryKey: ['users', 'me'],
