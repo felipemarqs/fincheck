e0eb6f71bb32bfabdfdb380e0db131a0b2f41b5f
felipemarq <137407401+felipemarq@users.noreply.github.com>
2024-05-24 17:03:10 +0000

    refactor: :recycle: Refactor
    Created query keys to padronize the code, and removed unused hook

--- frontend/src/app/hooks/useBankAccounts.ts
+++ frontend/src/app/hooks/useBankAccounts.ts
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { bankAccountService } from '../services/bankAccountService';
+import { queryKeys } from '../config/queryKeys';
 
 export const useBankAccounts = () => {
   const { data = [], isFetching } = useQuery({
-    queryKey: ['bankAccounts'],
+    queryKey: [queryKeys.BANK_ACCOUNTS],
     queryFn: bankAccountService.getAll,
     staleTime: Infinity,
   });
