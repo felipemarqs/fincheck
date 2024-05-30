3baba603a3a25de42cd6b67c4f8b22fd0b46d749
felipemarq <137407401+felipemarq@users.noreply.github.com>
2024-05-30 11:48:01 +0000

    fix: :children_crossing: Adds a button to confirm the date input improving the user experience

--- frontend/src/view/components/DatePickerInput.tsx
+++ frontend/src/view/components/DatePickerInput.tsx
@@ -4,22 +4,23 @@ import { formatDate } from '../../app/utils/formatDate';
 import { Popover } from './Popover';
 import { DatePicker } from './DatePicker';
 import { ErrorContainer } from './ErrorContainer';
+import { Button } from './Button';
 
 interface DatePickerInputProps {
   error?: string;
   className?: string;
+  placeholder?: string;
   value?: Date;
   onChange?(date: Date): void;
-  placeHolder?: string;
 }
 export const DatePickerInput = ({
   error,
   className,
   value,
+  placeholder,
   onChange,
-  placeHolder,
 }: DatePickerInputProps) => {
-  const [selectedDate, setSelectedDate] = useState(value);
+  const [selectedDate, setSelectedDate] = useState(value ?? new Date());
 
   const handleChangeDate = (date: Date) => {
     setSelectedDate(date);
@@ -39,17 +40,21 @@ export const DatePickerInput = ({
             )}
           >
             <span className="absolute text-gray-700 text-xs left-[13px] top-2 pointer-events-none">
-              {!placeHolder && 'Data'}
-              {placeHolder && placeHolder}
+              {!placeholder && <>Data</>}
+              {placeholder && placeholder}
             </span>
 
-            {selectedDate && <span>{formatDate(selectedDate)}</span>}
-            {!selectedDate && <span>Selecione uma data</span>}
+            <span>{formatDate(selectedDate)}</span>
           </button>
         </Popover.Trigger>
 
-        <Popover.Content>
+        <Popover.Content className="">
           <DatePicker value={selectedDate} onChange={handleChangeDate} />
+          <div className="w-full flex justify-end">
+            <Popover.Close>
+              <Button className="h-8 rounded-md px-3 text-xs">Ok</Button>
+            </Popover.Close>
+          </div>
         </Popover.Content>
       </Popover.Root>
       {error && <ErrorContainer error={error} />}
--- frontend/src/view/components/Popover.tsx
+++ frontend/src/view/components/Popover.tsx
@@ -1,6 +1,6 @@
-import * as RadixPopover from "@radix-ui/react-popover";
-import React from "react";
-import { cn } from "../../app/utils/cn";
+import * as RadixPopover from '@radix-ui/react-popover';
+import React from 'react';
+import { cn } from '../../app/utils/cn';
 
 const PopoverRoot = ({ children }: { children: React.ReactNode }) => {
   return <RadixPopover.Root>{children}</RadixPopover.Root>;
@@ -24,7 +24,7 @@ const PopoverContent = ({ children, className }: PopoverContentProps) => {
     <RadixPopover.Portal>
       <RadixPopover.Content
         className={cn(
-          "rounded-2xl p-4 bg-white space-y-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)] data-[side=bottom]:animate-slideUpAndFade data-[side=top]:animate-slideDownAndFade z-[99]",
+          'rounded-2xl p-4 bg-white space-y-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)] data-[side=bottom]:animate-slideUpAndFade data-[side=top]:animate-slideDownAndFade z-[99]',
           className
         )}
       >
@@ -34,8 +34,13 @@ const PopoverContent = ({ children, className }: PopoverContentProps) => {
   );
 };
 
+const PopoverClose = ({ children }: { children: React.ReactNode }) => {
+  return <RadixPopover.Close>{children}</RadixPopover.Close>;
+};
+
 export const Popover = {
   Root: PopoverRoot,
   Trigger: PopoverTrigger,
   Content: PopoverContent,
+  Close: PopoverClose,
 };
